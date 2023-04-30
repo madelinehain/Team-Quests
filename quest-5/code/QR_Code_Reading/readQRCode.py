3fe5ef970619c58657c69a4a269926ff47984613
Miguel Ianus-Valdivia <maiv@bu.edu>
2023-04-30 14:13:30 -0400

    Debug barcode reading

--- quest-5/code/QR_Code_Reading/readQRCode.py
+++ quest-5/code/QR_Code_Reading/readQRCode.py
@@ -6,10 +6,13 @@ import imutils
 import time
 import cv2
 
+# Detup CLI argument parser
 ap = argparse.ArgumentParser()
 ap.add_argument("-o", "--output", type=str, default="barcodes.csv",
-help="path to output CSV file containing barcodes")
+    help="path to output CSV file containing barcodes")
 args = vars(ap.parse_args())
+
+# Initialize the Pi cam video stream
 #vs = VideoStream(src=0).start()  #Uncomment this if you are using Webcam
 vs = VideoStream(usePiCamera=True).start() # For Pi Camera
 time.sleep(2.0)
@@ -18,7 +21,9 @@ found = set()
 
 print("Initialized")
 
+# Big ol' reccuring loop
 while True:
+    # Detect and decode QR code
     frame = vs.read()
     frame = imutils.resize(frame, width=400)
     barcodes = pyzbar.decode(frame)
@@ -31,7 +36,9 @@ while True:
         text = "{} ({})".format(barcodeData, barcodeType)
         print (text)
         cv2.putText(frame, text, (x, y - 10),
-        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        # Show the barcode and barcode information
+        cv2.imshow("Barcode Reader", frame)
 
     # if the barcode text is currently not in our CSV file, write
     # the timestamp + barcode to disk and update the set
@@ -42,10 +49,12 @@ while True:
         cv2.imshow("Barcode Reader", frame)
         key = cv2.waitKey(1) & 0xFF
 
-        # if the `s` key is pressed, break from the loop
-        if key == ord("s"):
-            break
-        print("[INFO] cleaning up...")
-        csv.close()
-        cv2.destroyAllWindows()
-        vs.stop()
+    # if the `s` key is pressed, break from the loop
+    if key == ord("s"):
+        break
+
+# Clean-up procedures
+print("[INFO] cleaning up...")
+csv.close()
+cv2.destroyAllWindows()
+vs.stop()
