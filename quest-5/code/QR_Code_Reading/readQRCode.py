c99f352505f8b7215b02bec32cd1dd351dc8290a
Miguel Ianus-Valdivia <maiv@bu.edu>
2023-04-29 20:44:47 -0400

    Fix indentations

--- quest-5/code/QR_Code_Reading/readQRCode.py
+++ quest-5/code/QR_Code_Reading/readQRCode.py
@@ -22,13 +22,13 @@ while True:
     barcodes = pyzbar.decode(frame)
     for barcode in barcodes:
         (x, y, w, h) = barcode.rect
-    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-    barcodeData = barcode.data.decode("utf-8")
-    barcodeType = barcode.type
-    text = "{} ({})".format(barcodeData, barcodeType)
-    print (text)
-    cv2.putText(frame, text, (x, y - 10),
-    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        barcodeData = barcode.data.decode("utf-8")
+        barcodeType = barcode.type
+        text = "{} ({})".format(barcodeData, barcodeType)
+        print (text)
+        cv2.putText(frame, text, (x, y - 10),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
     # if the barcode text is currently not in our CSV file, write
     # the timestamp + barcode to disk and update the set
