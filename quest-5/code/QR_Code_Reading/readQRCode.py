6bef930fffd98fc85adfe7c2772f8b96096f6b2e
Miguel Ianus-Valdivia <maiv@bu.edu>
2023-05-02 15:06:35 -0400

    Clear previous entry

--- quest-5/code/QR_Code_Reading/readQRCode.py
+++ quest-5/code/QR_Code_Reading/readQRCode.py
@@ -42,7 +42,8 @@ while True:
     # if the barcode text is currently not in our CSV file, write
     # the timestamp + barcode to disk and update the set
         # if barcodeData not in found:
-        csv.writerow(0)
+        csv.seek(0)
+        csv.truncate()
         csv.write("{},{}".format(datetime.datetime.now(), barcodeData))
         csv.flush()
         found.add(barcodeData)
