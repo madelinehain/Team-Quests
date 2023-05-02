f74a363f34fac74dd72764f8a02f7645dda26cc0
Madeline Hain <maddiech@bu.edu>
2023-05-01 22:56:46 -0400

    Fixed clients and servers

--- quest-5/code/QR_Code_Reading/Q5_QR_KeyFob.js
+++ quest-5/code/QR_Code_Reading/Q5_QR_KeyFob.js
@@ -5,47 +5,44 @@
   UDP Server for Thermistor, Video, & LED Button
   4/30/2023
 */
-// UDP Client
-var PORT = 33333;
-var HOST = '192.168.1.6';
-var dgram = require('dgram');
-var client = dgram.createSocket('udp4');
-// Read CSV File
-var fs = require('fs');
-var csv = require("csv-parse");
-// Send QR Code Scooter ID (SID)
-const send_QRcode_interval = 2000; // interval to send QR code data in miliseconds
 
+const dgram = require('dgram');
+var fs = require('fs');
+
+const HOST = '192.168.1.6';
+const PORT = 30000;
+const client = dgram.createSocket('udp4');
+var csv = require("csv-parse");
+
+var QR_data = [];  // Array to hold all csv data
 var SID_message = 'test1, test2';
 
-// On connection, send QR Code Data to Database Server
-setInterval(() => {
+///// WORKS
+// Read CSV File Into an Array
+fs.createReadStream('QR_Code_Data.csv')
+.pipe(csv.parse())      // fixed by using csv.parse() instead of csv()
+.on('data', (row) => {
+    console.log(row);
+    QR_data.push(row);  // Add row of data to array
+})
 
-    var QR_data = [];  // Array to hold all csv data
-
-    // Read CSV File Into an Array
-    fs.createReadStream('QR_Code_Data.csv')
-    .pipe(csv.parse())      // fixed by using csv.parse() instead of csv()
-    .on('data', (row) => {
-        console.log(row);
-        QR_data.push(row);  // Add row of data to array
-    })
-    .on('end', () => {
-        console.log('CSV file successfully processed');
-        // res.send(QR_data);  // Send array of data back to requestor
-    });
-
-    // var SID_message = QR_data[QR_data.length - 1].join('');
-
-    // var SID_message = '';
-    if (QR_data.length > 0 && QR_data[QR_data.length - 1] !== undefined) {
-        SID_message = QR_data[QR_data.length - 1].join('');
+.on('end', () => {
+    console.log('CSV file successfully processed');
+    console.log(QR_data.length)
+    console.log(QR_data)
+    if (QR_data.length > 0){ //&& QR_data[QR_data.length - 1] !== undefined) {
+        console.log('HELLO TEST');
+        SID_message = QR_data[QR_data.length - 1].join(', ');
+        console.log(SID_message)
     }
+    const message = new Buffer(SID_message);
 
-
-    client.send(SID_message, PORT, HOST, function(err) {
-    if (err) throw err;
-    console.log('UDP message (SID) sent to ' + HOST +':'+ PORT);
-    client.close();
+    client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+        if (err) throw err;
+        console.log('UDP message sent to ' + HOST +':'+ PORT);
+        client.close();
     });
-}, send_QRcode_interval);
+
+});
+
+
