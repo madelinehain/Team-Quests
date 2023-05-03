b6004a5954c5853a2bdd4c54bef4553110aaba6e
maxbak753 <maxbak753@gmail.com>
2023-05-02 20:36:24 -0400

    Q6 Server mhm

---
+++ quest-6/code/Q6_server_browser/Q6_server.js
@@ -0,0 +1,121 @@
+/*
+  Maxwell Bakalos, Miguel Ianus-Valdiva, Emre Karabay, Madeline Hain
+  EC444 Smart & Connected Systems
+  Quest 6 - Rally -- racing a course with checkpoints
+  UDP Server For Communicating Between Car & Webpage
+  5/2/2023
+*/
+
+// Required module
+var dgram = require('dgram');
+// Port and IP
+var PORT_CLIENT = 3333;          // Port where the thermistor data comes from
+var HOST = '192.168.1.6'; // Server Host Address (whatever is running this node server)
+const PORT = process.env.PORT || 8080;  // Port where server is running from "NodeServerHostIP:PORT"
+// Create socket
+var server = dgram.createSocket('udp4');
+
+// Read CSV File & Send Array to HTML File
+var express = require('express');
+var app = express();
+var path = require('path');
+var csv = require("csv-parse");
+// Create CSV File
+var fs = require('fs');
+// Button for LED
+const bodyParser = require('body-parser');
+
+const drive_control_send_interval = 500;    // interval (milliseconds) between sending driving commands
+var server_send_message = "Ok!"   // Message Sent to ESP32
+
+
+/////////////////////////////////////////////////////////////
+//  Receive Steering & Throttle Signal from HTML Buttons  //
+////////////////////////////////////////////////////////////
+
+// Direction Signals
+app.post('/leftButton', (req, res) => {
+    console.log("Left")
+    server_send_message = "Left"
+});
+app.post('/rightButton', (req, res) => {
+    console.log("Right")
+    server_send_message = "Right"
+});
+app.post('/forwardButton', (req, res) => {
+    console.log("Forward")
+    server_send_message = "Forward"
+});
+app.post('/reverseButton', (req, res) => {
+    console.log("Reverse")
+    server_send_message = "Reverse"
+});
+
+// Speed Signals
+app.post('/stopButton', (req, res) => {
+    console.log("Stop")
+    server_send_message = "Stop"
+});
+app.post('/slowButton', (req, res) => {
+    console.log("Slow")
+    server_send_message = "Slow"
+});
+app.post('/mediumButton', (req, res) => {
+    console.log("Medium")
+    server_send_message = "Medium"
+});
+app.post('/fastButton', (req, res) => {
+    console.log("Fast")
+    server_send_message = "Fast"
+});
+
+
+/////////////////////
+//  Server Stuff  //
+////////////////////
+
+// Create server that listens on a port
+server.on('listening', function () {
+    var address = server.address();
+    console.log('UDP Server listening on ' + address.address + ":" + address.port);
+});
+
+// Bind server to port and IP
+server.bind({
+    address: HOST,
+    port: PORT_CLIENT,
+    // exclusive: true
+});
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/Q6_browser.html')); // <-- HTML File for Webpage
+});
+
+// Serve static files from the 'public' directory (for jQuery)
+app.use(express.static('jQuery_File'))
+
+// Errors
+server.on('error', (err) => {
+    console.log(`server error:\n${err.stack}`);
+    server.close();
+  });
+
+// Start the Server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+// // Function to Periodically Send out Driving Control Commands
+// function send_drive_control() {
+//     // Send Driving Control Message
+//     server.send(server_send_message, PORT_CLIENT, remote.address, function(error){
+//         if(error){
+//           console.log('Driving Control Send Error!');
+//         }
+//         else{
+//           console.log('Sent: ' + server_send_message);
+//         }
+//     });
+// }
+// setInterval(send_drive_control, drive_control_send_interval);
