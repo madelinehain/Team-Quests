5ab0e2fa07f207dcfb5869d081e1dc72989d605c
maxbak753 <maxbak753@gmail.com>
2023-05-07 16:10:01 -0400

    AlphaDisplay ESP32 done probably

--- quest-6/code/Q6_AvgDisplayUDP/main/Q6_AvgDisplayUDP.c
+++ quest-6/code/Q6_AvgDisplayUDP/main/Q6_AvgDisplayUDP.c
@@ -1,8 +1,12 @@
 /*
-    Uhhh
+  Maxwell Bakalos, Miguel Ianus-Valdiva, Emre Karabay, Madeline Hain
+  EC444 Smart & Connected Systems
+  Quest 6 - Rally -- racing a course with checkpoints
+  ESP32 to Display Average Transit Time
+  5/7/2023
 */
 
-// Include for UDP Client
+// Include: UDP Client
 #include <string.h>
 #include <sys/param.h>
 #include "freertos/FreeRTOS.h"
@@ -18,9 +22,12 @@
 #include "lwip/sockets.h"
 #include "lwip/sys.h"
 #include <lwip/netdb.h>
-
-// Include for LED
-// #include <driver/gpio.h>
+// Include: Alphanumeric Display
+#include "alphanumTable.h"
+#include <string.h>         // for strings
+#include "driver/uart.h"
+#include "esp_vfs_dev.h"	// This is associated with VFS -- virtual file system interface and abstraction -- see the docs
+#include "driver/i2c.h"     // for Alphanumeric Display
 
 
 // DEFINE: UDP Client //////////////////////////////////// 
@@ -38,12 +45,32 @@
 
 #define PORT CONFIG_EXAMPLE_PORT
 
-// // DEFINE: LED Button //////////////////////////////////// 
-// #define LED_PIN 19
+// // DEFINE: Alphanumeric Display //////////////////////////////////// 
+// 14-Segment Alphanumeric Display
+#define SLAVE_ADDR                         0x70 // alphanumeric address
+#define OSC                                0x21 // oscillator cmd
+#define HT16K33_BLINK_DISPLAYON            0x01 // Display on cmd
+#define HT16K33_BLINK_OFF                  0    // Blink off cmd
+#define HT16K33_BLINK_CMD                  0x80 // Blink cmd
+#define HT16K33_CMD_BRIGHTNESS             0xE0 // Brightness cmd
+
+// Master I2C
+#define I2C_EXAMPLE_MASTER_SCL_IO          22   // gpio number for i2c clk
+#define I2C_EXAMPLE_MASTER_SDA_IO          23   // gpio number for i2c data
+#define I2C_EXAMPLE_MASTER_NUM             I2C_NUM_0  // i2c port
+#define I2C_EXAMPLE_MASTER_TX_BUF_DISABLE  0    // i2c master no buffer needed
+#define I2C_EXAMPLE_MASTER_RX_BUF_DISABLE  0    // i2c master no buffer needed
+#define I2C_EXAMPLE_MASTER_FREQ_HZ         100000     // i2c master clock freq
+#define WRITE_BIT                          I2C_MASTER_WRITE // i2c master write
+#define READ_BIT                           I2C_MASTER_READ  // i2c master read
+#define ACK_CHECK_EN                       true // i2c master will check ack
+#define ACK_CHECK_DIS                      false// i2c master will not check ack
+#define ACK_VAL                            0x00 // i2c ack value
+#define NACK_VAL                           0xFF // i2c nack value
 
 
 // GLOBAL VARIABLES: UDP Client //////////////////////////////////// 
-static const char *TAG = "LED_Client";
+static const char *TAG = "Display_Client";
 // static const char *payload = "Message from ESP32 ";
 char payload[140] = "aloha";
 
@@ -52,7 +79,7 @@ char rx_buffer[128];
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
-// FUNCTIONS: UDP Client ///////////////////////////////////////////////////////////////////////////////
+//  FUNCTIONS: UDP Client  /////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 static void udp_client_task(void *pvParameters)
@@ -127,17 +154,6 @@ static void udp_client_task(void *pvParameters)
                     break;
                 }
 
-                // // ADDED CODE //////
-                // if (strncmp(rx_buffer, "BUTTON_PRESSED", 10) == 0) {
-                //     ESP_LOGI(TAG, "LED BLINKING!!!");
-                //     blink_state = !(blink_state);           // flip LED state
-                //     gpio_set_level(LED_PIN, blink_state);   // turn on/off LED
-                // }
-                // else {
-                //     ESP_LOGI(TAG, "LED OFF");
-                //     gpio_set_level(LED_PIN, 0);  // turn off LED
-                // }
-
             }
 
             vTaskDelay(2000 / portTICK_PERIOD_MS);
@@ -153,36 +169,251 @@ static void udp_client_task(void *pvParameters)
 }
 
 
-// // Blink LED When HTML Page Button is Pressed
-// static void LED_task() {
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  FUNCTIONS: Alphanumeric Display & I2C  /////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//     short blink_state = 0;
+// Function to initiate i2c -- note the MSB declaration!
+static void i2c_example_master_init(){
+  // Debug
+  printf("\n>> i2c Config\n");
+  int err;
 
-//     while (1) 
-//     {
-//         // If the button has been pressed (ON)
-//         if (strncmp(rx_buffer, "BUTTON_PRESSED", 10) == 0) {
-//             ESP_LOGI(TAG, "LED BLINKING!!!");
-//             blink_state = !(blink_state);           // flip LED state
-//             gpio_set_level(LED_PIN, blink_state);   // turn on/off LED
-//         }
-//         // If the button is de-pressed (OFF)
-//         else {
-//             ESP_LOGI(TAG, "LED OFF");
-//             gpio_set_level(LED_PIN, 0);  // turn off LED
-//         }
+  // Port configuration
+  int i2c_master_port = I2C_EXAMPLE_MASTER_NUM;
 
-//             vTaskDelay(pdMS_TO_TICKS(500)); // delay
-//     }
-// }
+  /// Define I2C configurations
+  i2c_config_t conf;
+  conf.mode = I2C_MODE_MASTER;                              // Master mode
+  conf.sda_io_num = I2C_EXAMPLE_MASTER_SDA_IO;              // Default SDA pin
+  conf.sda_pullup_en = GPIO_PULLUP_ENABLE;                  // Internal pullup
+  conf.scl_io_num = I2C_EXAMPLE_MASTER_SCL_IO;              // Default SCL pin
+  conf.scl_pullup_en = GPIO_PULLUP_ENABLE;                  // Internal pullup
+  conf.master.clk_speed = I2C_EXAMPLE_MASTER_FREQ_HZ;       // CLK frequency
+  conf.clk_flags = 0;                                       // <-- UNCOMMENT IF YOU GET ERRORS (see readme.md)
+  err = i2c_param_config(i2c_master_port, &conf);           // Configure
+  if (err == ESP_OK) {printf("- parameters: ok\n");}
+
+  // Install I2C driver
+  err = i2c_driver_install(i2c_master_port, conf.mode,
+                      I2C_EXAMPLE_MASTER_RX_BUF_DISABLE,
+                      I2C_EXAMPLE_MASTER_TX_BUF_DISABLE, 0);
+  // i2c_set_data_mode(i2c_master_port,I2C_DATA_MODE_LSB_FIRST,I2C_DATA_MODE_LSB_FIRST);
+  if (err == ESP_OK) {printf("- initialized: yes\n\n");}
+
+  // Dat in MSB mode
+  i2c_set_data_mode(i2c_master_port, I2C_DATA_MODE_MSB_FIRST, I2C_DATA_MODE_MSB_FIRST);
+}
 
 
+// Utility  Functions //////////////////////////////////////////////////////////
 
-///////////////////////////////////////////////////////////////////////////////////////
-// MAIN MODULE ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+// Utility function to test for I2C device address -- not used in deploy
+int testConnection(uint8_t devAddr, int32_t timeout) {
+  i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+  i2c_master_start(cmd);
+  i2c_master_write_byte(cmd, (devAddr << 1) | I2C_MASTER_WRITE, ACK_CHECK_EN);
+  i2c_master_stop(cmd);
+  int err = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd, 1000 / portTICK_PERIOD_MS);
+  i2c_cmd_link_delete(cmd);
+  return err;
+}
 
-void app_main(void)
+// Utility function to scan for i2c device
+static void i2c_scanner() {
+  int32_t scanTimeout = 1000;
+  printf("\n>> I2C scanning ..."  "\n");
+  uint8_t count = 0;
+  for (uint8_t i = 1; i < 127; i++) {
+      // printf("0x%X%s",i,"\n");
+      if (testConnection(i, scanTimeout) == ESP_OK) {
+          printf( "- Device found at address: 0x%X%s", i, "\n");
+          count++;
+      }
+  }
+  if (count == 0)
+      printf("- No I2C devices found!" "\n");
+  printf("\n");
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+// Alphanumeric Functions //////////////////////////////////////////////////////
+
+// Turn on oscillator for alpha display
+int alpha_oscillator() {
+  int ret;
+  i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+  i2c_master_start(cmd);
+  i2c_master_write_byte(cmd, ( SLAVE_ADDR << 1 ) | WRITE_BIT, ACK_CHECK_EN);
+  i2c_master_write_byte(cmd, OSC, ACK_CHECK_EN);
+  i2c_master_stop(cmd);
+  ret = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd, 1000 / portTICK_PERIOD_MS);
+  i2c_cmd_link_delete(cmd);
+  vTaskDelay(200 / portTICK_PERIOD_MS);
+  return ret;
+}
+
+// Set blink rate to off
+int no_blink() {
+  int ret;
+  i2c_cmd_handle_t cmd2 = i2c_cmd_link_create();
+  i2c_master_start(cmd2);
+  i2c_master_write_byte(cmd2, ( SLAVE_ADDR << 1 ) | WRITE_BIT, ACK_CHECK_EN);
+  i2c_master_write_byte(cmd2, HT16K33_BLINK_CMD | HT16K33_BLINK_DISPLAYON | (HT16K33_BLINK_OFF << 1), ACK_CHECK_EN);
+  i2c_master_stop(cmd2);
+  ret = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd2, 1000 / portTICK_PERIOD_MS);
+  i2c_cmd_link_delete(cmd2);
+  vTaskDelay(200 / portTICK_PERIOD_MS);
+  return ret;
+}
+
+// Set Brightness
+int set_brightness_max(uint8_t val) {
+  int ret;
+  i2c_cmd_handle_t cmd3 = i2c_cmd_link_create();
+  i2c_master_start(cmd3);
+  i2c_master_write_byte(cmd3, ( SLAVE_ADDR << 1 ) | WRITE_BIT, ACK_CHECK_EN);
+  i2c_master_write_byte(cmd3, HT16K33_CMD_BRIGHTNESS | val, ACK_CHECK_EN);
+  i2c_master_stop(cmd3);
+  ret = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd3, 1000 / portTICK_PERIOD_MS);
+  i2c_cmd_link_delete(cmd3);
+  vTaskDelay(200 / portTICK_PERIOD_MS);
+  return ret;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+// Set-Up the Alphanumeric Display
+static void setup_alpha_display() {
+  i2c_example_master_init();  // Initial Set-Up
+  i2c_scanner();              // Scan to find I2C Device
+  // Debug
+  int ret;
+  printf(">> Test Alphanumeric Display: \n");
+
+  // Set up routines
+  // Turn on alpha oscillator
+  ret = alpha_oscillator();
+  if(ret == ESP_OK) {printf("- oscillator: ok \n");}
+  // Set display blink off
+  ret = no_blink();
+  if(ret == ESP_OK) {printf("- blink: off \n");}
+  ret = set_brightness_max(0xF);
+  if(ret == ESP_OK) {printf("- brightness: max \n");}
+}
+
+// Write Character Array to the Alphanumeric Display
+static void write_alpha_display(char bs[])
 {
+    // Variable Definitions
+    int ret;
+    int i_bs_0 = bs[0]; // buffer string character 1 (int)
+    int i_bs_1 = bs[1]; // buffer string character 2 (int)
+    int i_bs_2 = bs[2]; // buffer string character 3 (int)
+    int i_bs_3 = bs[3]; // buffer string character 4 (int)
+    uint16_t displaybuffer[8];    // display input variable
+
+    // Convert Characters to Alphanumeric Display Binary Values
+    displaybuffer[0] = alphafonttable[i_bs_0];
+    displaybuffer[1] = alphafonttable[i_bs_1];
+    displaybuffer[2] = alphafonttable[i_bs_2];
+    displaybuffer[3] = alphafonttable[i_bs_3];
+
+    // Send commands characters to display over I2C
+    i2c_cmd_handle_t cmd4 = i2c_cmd_link_create();
+    i2c_master_start(cmd4);
+    i2c_master_write_byte(cmd4, ( SLAVE_ADDR << 1 ) | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd4, (uint8_t)0x00, ACK_CHECK_EN);
+    for (uint8_t i=0; i<8; i++) {
+      i2c_master_write_byte(cmd4, displaybuffer[i] & 0xFF, ACK_CHECK_EN);
+      i2c_master_write_byte(cmd4, displaybuffer[i] >> 8, ACK_CHECK_EN);
+    }
+    i2c_master_stop(cmd4);
+    ret = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd4, 1000 / portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd4);
+
+    /*
+    if(ret == ESP_OK) {
+        printf("\nWrote: %s \n\n", bs);
+    }
+    */
+}
+
+
+// TASK --------------------------------------------/////////////////////////////
+// Write Average Transit Time to Alpha Display Task
+void write_display_task() {
+
+    char display_value[16] = "WAIT";
+
+    while(1) {
+        // If it ain't NULL
+        if (strtok(rx_buffer, ".") != NULL) {
+            // Get the Number excluding the decimal part
+            sprintf(display_value, "%s", strtok(rx_buffer, "."));
+        }
+
+        // Write to Alphanumeric Display
+        write_alpha_display(display_value);
+    }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  FUNCTIONS: Initialization & Main  //////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void message4startup(char startup_message[], float slide_time_length) {
+    /* 
+        startup_message    =  a text message that will slide across the display upon startup
+        slide_time_length  =  how long it wil take for the message to slide across the display
+    */
+    // # of characters in the message
+    int mes_len = strlen(startup_message);
+
+    // Get slide interval (between characters) in microseconds
+    int slide_interval = (slide_time_length /  mes_len) * 1000000;
+
+    // Preallocate strings
+    char disp_str[4];
+    char full_disp_str[mes_len+8];
+
+    // Create Full Display String
+    sprintf(full_disp_str, "    %s    ", startup_message);
+
+    // Roll the Message Across the Alphanumeric Display
+    for (int i=0; i<=(mes_len+4); i++) {
+        // Set Display Characters
+        disp_str[0] = full_disp_str[i];
+        disp_str[1] = full_disp_str[i+1];
+        disp_str[2] = full_disp_str[i+2];
+        disp_str[3] = full_disp_str[i+3];
+
+        // Show on Display
+        write_alpha_display(disp_str);
+        // Wait
+        usleep(slide_interval);
+        // printf("\n[%d] %s", i, disp_str);
+    }
+}
+
+// Initialization
+void initial_Setup() {
+    // Alphanumeric Display /////////////////////////////////
+    // Set SCK & SDA pins as pullups
+    gpio_set_pull_mode(22, GPIO_PULLUP_ONLY);
+    gpio_set_pull_mode(23, GPIO_PULLUP_ONLY);
+
+    // Initiate Alphanumeric Display
+    setup_alpha_display();
+
+    // Show Initial Message on Display
+    message4startup("HELLO    MY NAME IS ESP32", 5);
+    write_alpha_display("Wifi");
+    
+    // UDP Client ////////////////////////////////////////////
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
@@ -193,10 +424,18 @@ void app_main(void)
      */
     ESP_ERROR_CHECK(example_connect());
 
-    // // Initializing LED as Output
-    // gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+}
 
-//    xTaskCreate(udp_client_task, "udp_client", 4096, NULL, 5, NULL);
+
+///////////////////////////////////////////////////////////////////////////////////////
+// MAIN MODULE ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+void app_main(void)
+{   
+    // Run Initial Setup
+    initial_Setup();
+
+    // Tasks
     xTaskCreate(udp_client_task, "udp_client", 4096, NULL, configMAX_PRIORITIES, NULL);
-    // xTaskCreate(LED_task, "LED", 4096, NULL, configMAX_PRIORITIES-1, NULL);
+    xTaskCreate(write_display_task, "write_display", 2048, NULL, configMAX_PRIORITIES-1, NULL);
 }
