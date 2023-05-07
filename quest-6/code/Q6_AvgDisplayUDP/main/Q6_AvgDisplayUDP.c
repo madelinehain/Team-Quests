ff6d8e35efc5535d1d280db5d97b098122dd127b
maxbak753 <maxbak753@gmail.com>
2023-05-06 21:59:50 -0400

    Q6 code reading & display 1st

---
+++ quest-6/code/Q6_AvgDisplayUDP/main/Q6_AvgDisplayUDP.c
@@ -0,0 +1,202 @@
+/*
+    Uhhh
+*/
+
+// Include for UDP Client
+#include <string.h>
+#include <sys/param.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include <lwip/netdb.h>
+
+// Include for LED
+// #include <driver/gpio.h>
+
+
+// DEFINE: UDP Client //////////////////////////////////// 
+#ifdef CONFIG_EXAMPLE_SOCKET_IP_INPUT_STDIN
+#include "addr_from_stdin.h"
+#endif
+
+#if defined(CONFIG_EXAMPLE_IPV4)
+#define HOST_IP_ADDR CONFIG_EXAMPLE_IPV4_ADDR
+#elif defined(CONFIG_EXAMPLE_IPV6)
+#define HOST_IP_ADDR CONFIG_EXAMPLE_IPV6_ADDR
+#else
+#define HOST_IP_ADDR ""
+#endif
+
+#define PORT CONFIG_EXAMPLE_PORT
+
+// // DEFINE: LED Button //////////////////////////////////// 
+// #define LED_PIN 19
+
+
+// GLOBAL VARIABLES: UDP Client //////////////////////////////////// 
+static const char *TAG = "LED_Client";
+// static const char *payload = "Message from ESP32 ";
+char payload[140] = "aloha";
+
+// GLOBAL VARIABLES: LED ////////////////////////////////////
+char rx_buffer[128];
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+// FUNCTIONS: UDP Client ///////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+static void udp_client_task(void *pvParameters)
+{
+    char host_ip[] = HOST_IP_ADDR;
+    int addr_family = 0;
+    int ip_protocol = 0;
+
+    while (1) {
+
+#if defined(CONFIG_EXAMPLE_IPV4)
+        struct sockaddr_in dest_addr;
+        dest_addr.sin_addr.s_addr = inet_addr(HOST_IP_ADDR);
+        dest_addr.sin_family = AF_INET;
+        dest_addr.sin_port = htons(PORT);
+        addr_family = AF_INET;
+        ip_protocol = IPPROTO_IP;
+#elif defined(CONFIG_EXAMPLE_IPV6)
+        struct sockaddr_in6 dest_addr = { 0 };
+        inet6_aton(HOST_IP_ADDR, &dest_addr.sin6_addr);
+        dest_addr.sin6_family = AF_INET6;
+        dest_addr.sin6_port = htons(PORT);
+        dest_addr.sin6_scope_id = esp_netif_get_netif_impl_index(EXAMPLE_INTERFACE);
+        addr_family = AF_INET6;
+        ip_protocol = IPPROTO_IPV6;
+#elif defined(CONFIG_EXAMPLE_SOCKET_IP_INPUT_STDIN)
+        struct sockaddr_storage dest_addr = { 0 };
+        ESP_ERROR_CHECK(get_addr_from_stdin(PORT, SOCK_DGRAM, &ip_protocol, &addr_family, &dest_addr));
+#endif
+
+        int sock = socket(addr_family, SOCK_DGRAM, ip_protocol);
+        if (sock < 0) {
+            ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+            break;
+        }
+
+        // Set timeout
+        struct timeval timeout;
+        timeout.tv_sec = 10;
+        timeout.tv_usec = 0;
+        setsockopt (sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof timeout);
+
+        ESP_LOGI(TAG, "Socket created, sending to %s:%d", HOST_IP_ADDR, PORT);
+
+        while (1) {
+
+            int err = sendto(sock, payload, strlen(payload), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+            if (err < 0) {
+                ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+                break;
+            }
+            ESP_LOGI(TAG, "Message sent");
+
+            struct sockaddr_storage source_addr; // Large enough for both IPv4 or IPv6
+            socklen_t socklen = sizeof(source_addr);
+            int len = recvfrom(sock, rx_buffer, sizeof(rx_buffer) - 1, 0, (struct sockaddr *)&source_addr, &socklen);
+
+            sprintf(payload, "Received %s", rx_buffer);
+
+            // Error occurred during receiving
+            if (len < 0) {
+                ESP_LOGE(TAG, "recvfrom failed: errno %d", errno);
+                break;
+            }
+            // Data received
+            else {
+                rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string
+                ESP_LOGI(TAG, "Received %d bytes from %s:", len, host_ip);
+                ESP_LOGI(TAG, "%s", rx_buffer);
+                if (strncmp(rx_buffer, "OK: ", 4) == 0) {
+                    ESP_LOGI(TAG, "Received expected message, reconnecting");
+                    break;
+                }
+
+                // // ADDED CODE //////
+                // if (strncmp(rx_buffer, "BUTTON_PRESSED", 10) == 0) {
+                //     ESP_LOGI(TAG, "LED BLINKING!!!");
+                //     blink_state = !(blink_state);           // flip LED state
+                //     gpio_set_level(LED_PIN, blink_state);   // turn on/off LED
+                // }
+                // else {
+                //     ESP_LOGI(TAG, "LED OFF");
+                //     gpio_set_level(LED_PIN, 0);  // turn off LED
+                // }
+
+            }
+
+            vTaskDelay(2000 / portTICK_PERIOD_MS);
+        }
+
+        if (sock != -1) {
+            ESP_LOGE(TAG, "Shutting down socket and restarting...");
+            shutdown(sock, 0);
+            close(sock);
+        }
+    }
+    vTaskDelete(NULL);
+}
+
+
+// // Blink LED When HTML Page Button is Pressed
+// static void LED_task() {
+
+//     short blink_state = 0;
+
+//     while (1) 
+//     {
+//         // If the button has been pressed (ON)
+//         if (strncmp(rx_buffer, "BUTTON_PRESSED", 10) == 0) {
+//             ESP_LOGI(TAG, "LED BLINKING!!!");
+//             blink_state = !(blink_state);           // flip LED state
+//             gpio_set_level(LED_PIN, blink_state);   // turn on/off LED
+//         }
+//         // If the button is de-pressed (OFF)
+//         else {
+//             ESP_LOGI(TAG, "LED OFF");
+//             gpio_set_level(LED_PIN, 0);  // turn off LED
+//         }
+
+//             vTaskDelay(pdMS_TO_TICKS(500)); // delay
+//     }
+// }
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////
+// MAIN MODULE ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+void app_main(void)
+{
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+
+    // // Initializing LED as Output
+    // gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+
+//    xTaskCreate(udp_client_task, "udp_client", 4096, NULL, 5, NULL);
+    xTaskCreate(udp_client_task, "udp_client", 4096, NULL, configMAX_PRIORITIES, NULL);
+    // xTaskCreate(LED_task, "LED", 4096, NULL, configMAX_PRIORITIES-1, NULL);
+}
